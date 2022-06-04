e3ec7cfd08ed706a6594c7f57e6cc57c5f6e1b54
agkayster <ejike.chiboka@gmail.com>
2022-06-04 17:01:43 +0100

    feat: deployed springboot API to .jar and instance

--- src/main/java/com/example/demo/student/StudentController.java
+++ src/main/java/com/example/demo/student/StudentController.java
@@ -5,11 +5,13 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController // This allows the class function below it to serve RESTfUll endpoints//
@@ -43,9 +45,20 @@ public class StudentController {
     studentService.addNewStudent(student);
   }
 
+  // Here we use this method to Delete student information//
   @DeleteMapping(path = "{studentId}")
   public void deleteStudent(@PathVariable("studentId") Long studentId) {
     studentService.deleteStudent(studentId);
+  }
+
+  // We use this method to update information conserning student e.g. name and
+  // email//
+  @PutMapping(path = "{studentId}")
+  public void updateStudent(
+      @PathVariable("studentId") Long studentId,
+      @RequestParam(required = false) String name,
+      @RequestParam(required = false) String email) {
+    studentService.updateStudent(studentId, name, email);
   }
 
 }
--- src/main/java/com/example/demo/student/StudentService.java
+++ src/main/java/com/example/demo/student/StudentService.java
@@ -3,7 +3,10 @@ package com.example.demo.student;
 // import java.time.LocalDate;
 // import java.time.Month;
 import java.util.List;
+import java.util.Objects;
 import java.util.Optional;
+
+import javax.transaction.Transactional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 // import org.springframework.stereotype.Component;
@@ -24,6 +27,7 @@ public class StudentService {
     return studentRepository.findAll(); // Data been given from the service layer to the API layer (controller)//
   }
 
+  // This is the service to provide the Post method//
   public void addNewStudent(Student student) {
     Optional<Student> studentOptional = studentRepository.findStudentByEmail(student.getEmail());
     if (studentOptional.isPresent()) {
@@ -32,6 +36,7 @@ public class StudentService {
     studentRepository.save(student);
   }
 
+  // This is the service to provide the Delete method//
   public void deleteStudent(Long studentId) {
     // studentRepository.findById(studentId);
     boolean exists = studentRepository.existsById(studentId);
@@ -40,5 +45,22 @@ public class StudentService {
       throw new IllegalStateException("student with id " + studentId + " does not exist");
     }
     studentRepository.deleteById(studentId);
+  }
+
+  @Transactional
+  public void updateStudent(Long studentId, String name, String email) {
+    Student student = studentRepository.findById(studentId)
+        .orElseThrow(() -> new IllegalStateException("student with id " + studentId + " does not exist"));
+    if (name != null && name.length() > 0 && !Objects.equals(student.getName(), name)) {
+      student.setName(name);
+    }
+
+    if (email != null && email.length() > 0 && !Objects.equals(student.getEmail(), email)) {
+      Optional<Student> studentOptional = studentRepository.findStudentByEmail(email);
+      if (studentOptional.isPresent()) {
+        throw new IllegalStateException("email taken");
+      }
+      student.setEmail(email);
+    }
   }
 }
